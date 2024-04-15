27d324b69fd9a261e81ad94acf19a703ddbc0c4d
Michał Kurek <Vagab0nd@users.noreply.github.com>
2024-04-15 14:21:02 +0200

    Add anime xml  mapping test.

--- AniDb.Api.Test/AniDbHttpApiTests.cs
+++ AniDb.Api.Test/AniDbHttpApiTests.cs
@@ -1,19 +1,39 @@
+using AniDb.Api.Models.Anime;
+using AniDb.Api.Test.Resources;
 using FluentAssertions;
+using Flurl.Http.Testing;
+using Flurl.Http.Xml;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 using System.Threading.Tasks;
+using System.Xml.Serialization;
 
 namespace AniDb.Api.Test
 {
     [TestClass]
     public class AniDbHttpApiTests
     {
-        private IAniDbHttpApi target = new AniDbHttpApi();
+        private readonly IAniDbHttpApi target = new AniDbHttpApi();
 
         public TestContext TestContext { get; set; }
 
         [TestMethod]
         public async Task GetAnime_should_return_anime()
         {
+            //act
+            var response = await this.target.GetAnime(17709);
+
+            //assert
+            response.Should().NotBeNull();
+        }
+
+        [TestMethod]
+        public async Task GetAnime_should_deserialize_anime_xml()
+        {
+            //arrange
+            using var httpTest = new HttpTest();
+            var xmlResponse = ResourcesHelper.GetStringResource("AnimeResponse.txt");
+            httpTest.RespondWith(xmlResponse);
+
             //act
             var response = await this.target.GetAnime(17709);
 
---
+++ AniDb.Api.Test/Resources/ResourcesHelper.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.IO;
+using System.Reflection;
+
+namespace AniDb.Api.Test.Resources
+{
+    internal static class ResourcesHelper
+    {
+        public static string GetStringResource(string fileName)
+        {
+            var assembly = Assembly.GetExecutingAssembly();
+            using Stream stream = assembly.GetManifestResourceStream($"AniDb.Api.Test.Resources.{fileName}") ?? throw new InvalidOperationException($"File {fileName} not found.");
+            using StreamReader reader = new(stream);
+            return reader.ReadToEnd();
+        }
+    }
+}
