e89843c26721fedbfe0c3a9d7984c9b62547bf7c
Michał Kurek <Vagab0nd@users.noreply.github.com>
2024-04-21 20:48:48 +0200

    Add anime titles integration test

--- AniDb.Api.Test/AniDbAnimeTitlesIntegrationTests.cs
+++ AniDb.Api.Test/AniDbAnimeTitlesIntegrationTests.cs
@@ -1,10 +1,7 @@
-using AniDb.Api.Test.Resources;
 using FluentAssertions;
 using Flurl.Http.Testing;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 using System;
-using System.Collections.Generic;
-using System.Text;
 using System.Threading.Tasks;
 
 namespace AniDb.Api.Test
@@ -20,26 +17,12 @@ namespace AniDb.Api.Test
         public async Task GetAnimeTitles_should_return_anime_titles()
         {
             //arrange
-            using var httpTest = new HttpTest();
-            var fileResponse = ResourcesHelper.GetByteResource("anime-titles.xml.gz");
-            var headers = new Dictionary<string, string>
-            {
-                { "Content-Encoding", "gzip" }
-            };
-            httpTest.RespondWith(fileResponse, headers: headers);
 
             //act
-            try
-            {
-                var response = await this.target.GetAnimeTitles();
+            var response = await this.target.GetAnimeTitles();
 
-                //assert
-                response.Should().NotBeNull();
-            }
-            catch(Exception) 
-            {
-                throw new Exception();
-            }
+            //assert
+            response.Should().NotBeNull();
         }
     }
 }
