5ca656e608dea06d2f3e161496c0b8ee27687158
Michał Kurek <Vagab0nd@users.noreply.github.com>
2024-05-02 12:14:27 +0200

    GetAniDbResponse

--- AniDb.Api/Infrastracture/UrlExtensions.cs
+++ AniDb.Api/Infrastracture/UrlExtensions.cs
@@ -1,4 +1,7 @@
 ﻿using Flurl;
+using Flurl.Http;
+using Flurl.Http.Xml;
+using AniDb.Api.Models.Common;
 
 namespace AniDb.Api.Infrastracture
 {
@@ -14,6 +17,32 @@ namespace AniDb.Api.Infrastracture
             url.AppendQueryParam("clientver", ClientVersion);
             url.AppendQueryParam("protover", ProtocolVersion);
             return url;
+        }
+
+        //TODO: "Peek" root element in xml to determine if error.
+        public static async Task<Response<T>> GetAniDbResponse<T>(this Url url, CancellationToken cancellationToken = default) where T: class
+        {
+            var response = new FlurlRequest(url).GetAsync(cancellationToken: cancellationToken);
+
+            try
+            {
+                var error = await response.ReceiveXml<Error>();
+                return new Response<T>
+                {
+                    ErrorMessage = error.Text
+                };
+            }
+            catch
+            {
+                var data = await response.ReceiveXml<T>();
+                return data == null
+                    ? throw new InvalidOperationException("AniDB xml data is null")
+                    : new Response<T>
+                {
+                    Data = data
+                };
+            }
+
         }
     }
 }
