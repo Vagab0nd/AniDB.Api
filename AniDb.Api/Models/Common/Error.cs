173afc1bb5ac395c6af8b448fbbd0bdac0e068ea
Michał Kurek <Vagab0nd@users.noreply.github.com>
2024-05-02 12:14:07 +0200

    Add error

---
+++ AniDb.Api/Models/Common/Error.cs
@@ -0,0 +1,11 @@
+﻿using System.Xml.Serialization;
+
+namespace AniDb.Api.Models.Common
+{
+    [XmlRoot(ElementName = "error")]
+    public record Error
+    {
+        [XmlText]
+        public string Text { get; set; } = string.Empty;
+    }
+}
