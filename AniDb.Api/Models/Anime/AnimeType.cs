4d507181e620e7fad8cd22b7ed188a77fc23fe84
Michał Kurek <Vagab0nd@users.noreply.github.com>
2024-05-02 19:20:25 +0200

    AnimeType

---
+++ AniDb.Api/Models/Anime/AnimeType.cs
@@ -0,0 +1,19 @@
+﻿using System.Xml.Serialization;
+
+namespace AniDb.Api.Models.Anime
+{
+    public enum AnimeType
+    {
+        [XmlEnum(Name = "TV Series")]
+        TvSeries,
+        [XmlEnum(Name = "OVA")]
+        Ova,
+        Movie,
+        Other,
+        Web,
+        [XmlEnum(Name = "TV Special")]
+        TvSpecial,
+        [XmlEnum(Name = "Music Video")]
+        MusicVideo
+    }
+}
