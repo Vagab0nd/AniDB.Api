9712d630306ec911b4fec4e1042239de2c22ac69
Michał Kurek <Vagab0nd@users.noreply.github.com>
2024-07-28 14:13:54 +0200

    AniDbUdpClient

---
+++ AniDb.Api.Udp/AniDbUdpClient.cs
@@ -0,0 +1,24 @@
+﻿using System.Net;
+using System.Net.Sockets;
+
+namespace AniDb.Api.Udp
+{
+    public class AniDbUdpClient : IDisposable
+    {
+        private const string BaseUri = "api.anidb.net";
+        private const int RemotePort = 9000;
+
+        private readonly UdpClient client;
+
+        public AniDbUdpClient(int localPort)
+        {
+            this.client = new UdpClient(new IPEndPoint(IPAddress.Any, localPort));
+            this.client.Connect(BaseUri, RemotePort);
+        }
+
+        public void Dispose()
+        {
+            this.client?.Dispose();
+        }
+    }
+}
