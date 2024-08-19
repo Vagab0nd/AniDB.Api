073bbfc035298411ba4501b5a5e4b0a652ec2eb6
Michał Kurek <Vagab0nd@users.noreply.github.com>
2024-08-19 14:49:42 +0200

    Add implementation stubs

--- AniDb.Api.Udp/AniDbUdpApi.cs
+++ AniDb.Api.Udp/AniDbUdpApi.cs
@@ -7,12 +7,52 @@
             throw new NotImplementedException();
         }
 
+        public object BuddyAccept(int userId)
+        {
+            throw new NotImplementedException();
+        }
+
+        public object BuddyAdd(int buddyId, string buddyName)
+        {
+            throw new NotImplementedException();
+        }
+
+        public object BuddyDelete(int buddyId)
+        {
+            throw new NotImplementedException();
+        }
+
+        public object BuddyDeny(int userId)
+        {
+            throw new NotImplementedException();
+        }
+
+        public object BuddyList(int? startAt)
+        {
+            throw new NotImplementedException();
+        }
+
+        public object BuddyState(int? startAt)
+        {
+            throw new NotImplementedException();
+        }
+
         public object Encrypt()
         {
             throw new NotImplementedException();
         }
 
         public void Logout()
+        {
+            throw new NotImplementedException();
+        }
+
+        public object NotificationAdd(string? animeId, string? groupId, int type, int priority)
+        {
+            throw new NotImplementedException();
+        }
+
+        public object NotificationDelete(string? animeId, string? groupId)
         {
             throw new NotImplementedException();
         }
